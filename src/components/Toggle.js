fc683a0033d90659418145520b69aa71a4083d8f
Mohamad Karbejha <78583194+Karbejha@users.noreply.github.com>
2023-11-13 19:56:26 +0300

    add some features to Toggle

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn,setItOn] = useState(false);
 }
-
+function handleClick(){
+  setItOn((isOn)=>!isOn);
+}
+const color = isOn ? "red":"white";
+return <button 
+        style={{background:color}}
+        onClick={handleClick}>{isOn ? "ON":"OFF"}</button>
 
 export default Toggle;
